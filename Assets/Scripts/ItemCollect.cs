30530f2b1bcad133b947e029ec8f03aa632060f4
Ismael Neto <ismaelm.neto14@gmail.com>
2026-05-26 15:27:17 -0300

    script de item

--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -4,17 +4,29 @@ using TMPro;
 
 public class ItemCollect : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    [Header("Conteudo do artefato")]
+    public string titulo;
 
-    public ScriptableObject itemData;
+    [TextArea(4, 10)]
+    public string descricao;
+
+    [Header("Painel de Informação")]
     public Canvas canvas;
-    public TextMeshProUGUI TextMeshPro;
+    public TMP_Text textoPainel;
 
+    [Header("Narração")]
+    public AudioClip audioNarracao;
+    public AudioSource audioSource;
+    public bool tocarAudioAoInteragir = true;
+    public bool paraAudioAoSoltar = true;
 
     void Start()
     {
-        canvas.enabled = false;
-        TextMeshPro.text = "Oi, testando";
+        if (canvas != null) canvas.enabled = false;
+        if (audioSource != null) audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
     }
 
     // Update is called once per frame
@@ -25,6 +37,21 @@ public class ItemCollect : MonoBehaviour
 
     public void ShowCanvas()
     {
-        canvas.enabled = true;
+        if (textoPainel != null) textoPainel.text = "<b>" + titulo + "</b>\n\n" + descricao;
+
+        if (canvas != null) canvas.enabled = true;
+
+        if (tocarAudioAoInteragir && audioNarracao != null && audioSource != null)
+        {
+            audioSource.Stop();
+            audioSource.clip = audioNarracao;
+            audioSource.play();
+        }
+    }
+
+    public void HideCanvas()
+    {
+        if (canvas != null) canvas.enabled = false;
+        if (paraAudioAoSoltar && audioSource != null && audioSource.isPlaying) audioSource.Stop();
     }
 }
